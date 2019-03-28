3981a16a92dfecff8d6931e0840092fd27b44b6a
Mashuq Rahman <mashuq@gmail.com>
2019-03-28 14:37:56 +0600

    Added all starwars schema and resolver functionality

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,10 +1,40 @@
 const knex = require('../knex/knex');
+//knex.on('query', console.log);
 
 const resolvers = {
   Query: {
     persons: () => knex.select().table('person'),
-    planets: () => knex.select().table('planet'),
+    specieses: () => knex.select().table('species'),    
+    planets: () => knex.select().table('planet'),    
+    starships: () => knex.select().table('starship'),
+    vehicles: () => knex.select().table('vehicle'),
+
+    person: (root, args, context) => knex.select().table('person').where('id', args.id).first(),   
+    species: (root, args, context) => knex.select().table('species').where('id', args.id).first(),   
+    planet: (root, args, context) => knex.select().table('planet').where('id', args.id).first(),    
+    starship: (root, args, context) => knex.select().table('starship').where('id', args.id).first(),    
+    vehicle: (root, args, context) => knex.select().table('vehicle').where('id', args.id).first(),     
   },
+  Person: {
+    homeworld: (root, args, context) => knex.select().table('planet').where('id', root.homeworld).first(),
+    specieses: (root, args, context) => knex.select().table('species').join('species_person', 'species_id', '=', 'species.id').where('species_person.person_id', root.id),    
+    starships: (root, args, context) => knex.select().table('starship').join('starship_pilot', 'starship_id', '=', 'starship.id').where('starship_pilot.person_id', root.id),
+    vehicles: (root, args, context) => knex.select().table('vehicle').join('vehicle_pilot', 'vehicle_id', '=', 'vehicle.id').where('vehicle_pilot.person_id', root.id),
+  },
+  Species: {
+    persons: (root, args, context) => knex.select().table('person').join('species_person', 'person_id', '=', 'person.id').where('species_person.species_id', root.id),
+    planet: (root, args, context) => knex.select().table('planet').where('id', root.homeworld).first(), 
+  },
+  Planet: {
+    persons: (root, args, context) => knex.select().table('person').where('homeworld', root.id),
+    specieses: (root, args, context) => knex.select().table('species').where('homeworld', root.id), 
+  },
+  Starship: {
+    persons: (root, args, context) => knex.select().table('person').join('starship_pilot', 'person_id', '=', 'person.id').where('starship_pilot.starship_id', root.id),
+  },
+  Vehicle: {
+    persons: (root, args, context) => knex.select().table('person').join('vehicle_pilot', 'person_id', '=', 'person.id').where('vehicle_pilot.vehicle_id', root.id),
+  }
 };
 
 module.exports = resolvers;
--- graphql/schema.js
+++ graphql/schema.js
@@ -11,8 +11,29 @@ let schema = gql(`
     eye_color: String
     birth_year: String
     gender: String
+    homeworld: Planet
+    specieses : [Species]
+    starships: [Starship]
+    vehicles: [Vehicle]
+  }
+
+  type Species {
+    id: ID
+    name: String
+    classification: String
+    designation: String
+    average_height: Int
+    hair_colors: String
+    skin_colors: String
+    eye_colors: String
+    birth_year: String
+    average_lifespan: Int
+    homeworld: Planet
+    language: String
+    persons: [Person]
     planet: Planet
   }
+
 
   type Planet {
     id: ID,
@@ -25,11 +46,56 @@ let schema = gql(`
     terrain: String
     surface_water: Int
     population: Int
+    persons: [Person]
+    specieses: [Species]
+  }
+
+  type Starship{
+    id: ID,
+    name: String,
+    model: String,
+    manufacturer: String,
+    cost_in_credits: Float,
+    length: Int
+    max_atmosphering_speed: Int
+    crew: Int
+    passengers: Int
+    cargo_capacity: Float,
+    consumables: Float,
+    hyperdrive_rating: Float,
+    mglt: Int
+    starship_class: String
+    persons: [Person]
+  }
+
+  type Vehicle{
+    id: ID,
+    name: String,
+    model: String,
+    manufacturer: String,
+    cost_in_credits: Float,
+    length: Int
+    max_atmosphering_speed: Int
+    crew: Int
+    passengers: Int
+    cargo_capacity: Float,
+    consumables: Float,
+    vehicle_class: String,
+    persons: [Person]
   }
 
   type Query {
     persons: [Person],
+    specieses: [Species],
     planets: [Planet],
+    starships: [Starship],    
+    vehicles: [Vehicle],
+    
+    person(id: ID!): Person
+    planet(id: ID!): Planet
+    starship(id: ID!): Starship
+    vehicle(id: ID!): Vehicle
+    species(id: ID!): Species
   }
 `);
 
